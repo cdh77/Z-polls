ac4acd5e629ddebf1973e28dbb71b8ca06364650
cdh77 <orbitjazzy61@gmail.com>
2020-08-07 04:01:00 +0000

    Merge pull request #6 from IgnisDa/master
    PR#6

--- polls/urls.py
+++ polls/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('add-question/', views.AddQuestionView.as_view(), name='add-question'),
+    path('add-choice/<int:pk>/', views.AddChoicesView.as_view(), name='add-choice'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
